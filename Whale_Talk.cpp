292c8b4dc2886d3c5bf4347a76042b828eb1debc
Brian <brianmo3530@gmail.com>
2026-01-11 01:43:40 -0600

    Create Whale_Talk.cpp
    preparing the vowels that whales can make and preparing for the for loops and if statements. part 1 of 2

---
+++ Whale_Talk.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main() {
+    std::string input = "turpentine and turtles";
+    std::vector<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+    std::vector<char> result;
